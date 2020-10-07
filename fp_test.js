7ac1d77405f773f0a00323829f43ce6f1fb9cd49
Álvaro Sánchez <forensor@hotmail.com>
2020-10-07 15:31:56 +0200

    Update fp_test.js

--- fp_test.js
+++ fp_test.js
@@ -1,4 +1,3 @@
-
 /**
  * Returns an element from a list
  * 
@@ -8,13 +7,9 @@
  * 
  * @returns {any}
  */
-
 const find = ([x, ...xs], pos) => 
     pos === 0 ? x
   : find(xs, pos - 1)
-
-// Example: finding an element in a 2D array: find(find([x, ...xs], pos), pos)
-
 /**
  * Replaces an element from a list
  * 
@@ -25,14 +20,27 @@ const find = ([x, ...xs], pos) =>
  * 
  * @returns {any[]}
  */
-
 const replace = ([x, ...xs], pos, rep) => 
     pos === 0 ? [rep].concat(xs)
   : [x].concat(replace(xs, pos - 1, rep))
-
-// Example: replacing an element in a 2D array: 
-// replace(
-//   [x, ...xs], 
-//   pos, 
-//   replace(find([x, ...xs], pos), pos, rep)
-// )
+/**
+ * Returns the found element in a 2D list
+ * 
+ * @param {any[][]} arr 2D array filled with elements
+ * @param {number} row Desired row to work with
+ * @param {number} col Desired column
+ * 
+ * @returns {any}
+ */
+const find2D = (arr, row, col) => 
+  find(find(arr, row), col)
+/**
+ * Replaces an element in a 2D list
+ * 
+ * @param {any[][]} arr 2D array to be replaced
+ * @param {number} row Row to replace
+ * @param {number} col Column to replace
+ * @param {any} rep Replacement
+ */
+const replace2D = (arr, row, col, rep) => 
+  replace(arr, row, replace(find(arr, row), col, rep))
