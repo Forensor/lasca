f555656d9ad8c7e97188ed2fafb359ff2159a005
Álvaro Sánchez <forensor@hotmail.com>
2020-10-08 14:22:08 +0200

    Update fp_test.js

--- fp_test.js
+++ fp_test.js
@@ -1,11 +1,11 @@
 /**
  * Returns an element from a list
  * 
- * @param {any}      x First element of the list
- * @param {any[]}   xs Rest of the list
+ * @param {string}      x First element of the list
+ * @param {string[]}   xs Rest of the list
  * @param {number} pos Position to return
  * 
- * @returns {any}
+ * @returns {string}
  */
 const find = ([x, ...xs], pos) => 
     pos === 0 ? x
@@ -13,12 +13,12 @@ const find = ([x, ...xs], pos) =>
 /**
  * Replaces an element from a list
  * 
- * @param {any}      x First element of the list
- * @param {any[]}   xs Rest of the list
+ * @param {string}      x First element of the list
+ * @param {string[]}   xs Rest of the list
  * @param {number} pos The position of the list that's going to be replaced
- * @param {any}    rep The replacement
+ * @param {string}    rep The replacement
  * 
- * @returns {any[]}
+ * @returns {string[]}
  */
 const replace = ([x, ...xs], pos, rep) => 
     pos === 0 ? [rep].concat(xs)
@@ -26,35 +26,35 @@ const replace = ([x, ...xs], pos, rep) =>
 /**
  * Returns the found element in a 2D list
  * 
- * @param {any[][]} arr 2D array filled with elements
+ * @param {string[][]} arr 2D array filled with strings
  * @param {number}  row Desired row to work with
  * @param {number}  col Desired column
  * 
- * @returns {any}
+ * @returns {string}
  */
 const find2D = (arr, row, col) => 
   find(find(arr, row), col)
 /**
  * Replaces an element in a 2D list
  * 
- * @param {any[][]} arr 2D array to be replaced
+ * @param {string[][]} arr 2D array to be replaced
  * @param {number}  row Row to replace
  * @param {number}  col Column to replace
- * @param {any}     rep Replacement
+ * @param {string}     rep Replacement
  * 
- * @returns {any[][]}
+ * @returns {string[][]}
  */
 const replace2D = (arr, row, col, rep) => 
   replace(arr, row, replace(find(arr, row), col, rep))
 /**
  * Returns the specified string index inside a list
  * 
- * @param {string[][][]} arr 2D array filled with strings
+ * @param {string[][]} arr 2D array filled with strings
  * @param {number}       row Row
  * @param {number}       col Column
  * @param {number}       pos Index of the string
  * 
- * @returns
+ * @returns {string}
  */
 const find3D = (arr, row, col, pos) => 
   find(find2D(arr, row, col), pos)
@@ -62,13 +62,13 @@ const find3D = (arr, row, col, pos) =>
  * Returns a new list with a '' in its original place and the
  * old element being in the destination
  * 
- * @param {any[][]} arr 2D array to modify
+ * @param {string[][]} arr 2D array to modify
  * @param {number}   oR Origin row
  * @param {number}   oC Origin column
  * @param {number}   dR Destination row
  * @param {number}   dC Destination column
  * 
- * @returns {any[][]}
+ * @returns {string[][]}
  */
 const move = (arr, oR, oC, dR, dC) => 
   replace2D(replace2D(arr, dR, dC, find2D(arr, oR, oC)), oR, oC, '')
