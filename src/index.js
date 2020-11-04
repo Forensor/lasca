f3ab659870f47959a4616af9a16e65175217cff7
Álvaro Sánchez <forensor@hotmail.com>
2020-11-04 03:27:06 +0100

    User registration done

--- src/index.js
+++ src/index.js
@@ -2,28 +2,46 @@
 const express = require('express');
 const socketio = require('socket.io');
 const path = require('path');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const bcrypt = require('bcrypt');
-const sqlite3 = require('sqlite3').verbose();
+const sqliteAsync = require('sqlite-async');
 
 // App initialization
 const app = express();
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Database
-let db = new sqlite3.Database('LASCA', (err) => {
-  err ? console.log(err.message) : console.log('DB connected');;
-});
+const databaseCreation = async () => {
+  let db;
+  try {
+    db = await sqliteAsync.open('LASCA.sqlite3');
+    console.log('DB LASCA ready');
+  } catch (err) {
+    console.log(err.message);
+  }
+  
+  try {
+    await db.run(
+      `CREATE TABLE IF NOT EXISTS USER (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL
+      )`
+    );
+    console.log('TABLE USER ready');
+  } catch (err) {
+    console.log(err.message);
+  }
+  
+  console.log('DB operational, closing now...');
+  db.close();
+};
 
-db.run(`
-  CREATE TABLE IF NOT EXISTS USER (
-    id INTEGER PRIMATY KEY AUTOINCREMENT,
-    nickname text NOT NULL,
-    password text NOT NULL
-  )
-`, (err) => {
-  err ? console.log(err.message) : console.log(`User created: ${this.lastID}`);
-});
-
-db.close();
+databaseCreation();
 
 // Settings
 app.set('port', process.env.PORT || 3000);
@@ -33,23 +51,53 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Server start
 const server = app.listen(app.get('port'), () => {
-  console.log(`Server port: ${app.get('port')}`);
+  console.log('Server port:', app.get('port'));
 });
 
 // IO initialization
 const io = socketio.listen(server);
 
 // Routes
-app.get('/register', (req, res) => {
-  res.json({
-    usuario: "mipana",
-    password: bcrypt.hashSync('hola', 10)
-  });
+app.post('/ur', async (req, res) => {
+  let db;
+  let registered;
+  const username = req.body.username;
+  const password = req.body.password;
+  try {
+    db = await sqliteAsync.open('LASCA.sqlite3');
+  } catch (err) {
+    console.log(err.message);
+  }
+  try {
+    registered = await db.get(
+      'SELECT * FROM USER WHERE username = ?', 
+      [username]
+    );
+  } catch (err) {
+    console.log(err.message);
+  }
+
+  res.redirect('/');
+
+  if (registered) {
+    console.log('User already registered');
+  } else {
+    try {
+      await db.run(
+        'INSERT INTO USER(username, password) VALUES(?, ?)', 
+        [username, bcrypt.hashSync(password, 10)]
+      );
+      console.log(username, 'registered');
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+  db.close();
 });
 
 // Websockets
 io.on('connection', (socket) => {
-  console.log(`Client ${socket.id} connected`);
+  console.log('Client', socket.id, 'connected');
 
   socket.on('message', (data) => {
     console.log(data);
